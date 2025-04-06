f758a658861af1a3885ebeba2581508ee43b86a4
Abhinandan Kumar <abhinandan.kumar@uTrades-MacBook-Air.local>
2025-04-06 13:43:47 +0530

    App name updated

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hand Cricket Game',
       theme: ThemeData(
         // This is the theme of your application.
         //
